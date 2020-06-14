7407f780a1d4db27a9b3101dc488a0d530bad928
Zayidu Ansari <zayidu11@gmail.com>
2020-06-14 23:08:00 +0400

    Footer Copyrights and Creator

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,8 +10,15 @@ const Footer = () => {
         <img alt="NBA Logo" src="/images/nba_logo.png" />
       </Link>
       <div className={styles.right}>
-        @NBA {CURRENT_YEAR} All rights reserved.
+        @NBA {CURRENT_YEAR} All rights reserved.{' '}
       </div>
+      <a
+        href="https://github.com/zayidu/zDevConnector"
+        target="_blank"
+        className={styles.right}
+      >
+        ©Creator : zayidu
+      </a>
     </div>
   );
 };
